ba002708b50e06df453a828174e8b265f3d6684f
prachichikshe <pchikshe12@gmail.com>
2017-06-09 15:10:32 +0530

    Create assign13.py

---
+++ assign13.py
@@ -0,0 +1,9 @@
+#Write a Python program to combine each line from first file with the corresponding line in second file.
+
+with open('abc.txt') as fh1, open('pc.txt') as fh2:  
+           for line1, line2 in zip(fh1, fh2):  
+               # line1 from abc.txt, line2 from pc.txtg  
+                 print(line1+line2)  
+                 
+  '''Output
+  '''

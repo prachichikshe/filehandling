4def318f069d8529e5e2002ca6e239c8c1786dec
prachichikshe <pchikshe12@gmail.com>
2017-06-09 15:59:50 +0530

    Create assign12.py

--- assign12.py
+++ assign12.py
@@ -6,5 +6,9 @@ os.system('touch ab.txt')
 copyfile('pc.txt', 'ab.txt')  
 
 '''Output
+Paris is the capital and most populous city of France.
+By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts.
+It is the second largest metropolitan area in the European Union. 
+Paris is the fifth most expensive city in the world for luxury housing.
 '''
 

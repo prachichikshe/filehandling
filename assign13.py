f20cc9965587122efa5fb196f02a45f2666cb3c2
prachichikshe <pchikshe12@gmail.com>
2017-06-09 15:11:53 +0530

    Create assign13.py

--- assign13.py
+++ assign13.py
@@ -6,4 +6,15 @@ with open('abc.txt') as fh1, open('pc.txt') as fh2:
                  print(line1+line2)  
                  
   '''Output
+  Paris is the capital and most populous city of France.
+  Paris is the capital and most populous city of France.
+  
+  By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts.
+  By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts.
+  
+  It is the second largest metropolitan area in the European Union.
+  It is the second largest metropolitan area in the European Union. 
+  
+  Paris is the fifth most expensive city in the world for luxury housing.
+  Paris is the fifth most expensive city in the world for luxury housing.
   '''

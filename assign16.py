b2886761825c978d9da0264fffd90adde89ebb87
prachichikshe <pchikshe12@gmail.com>
2017-06-09 15:46:58 +0530

    Create assign16.py

---
+++ assign16.py
@@ -0,0 +1,8 @@
+#Q.16  Write a Python program to remove newline characters from a file.
+
+def remove_newlines(fname):  
+    flist = open(fname).readlines()  
+    return [s.rstrip('\n') for s in flist]  
+print(remove_newlines("test.txt"))  
+
+'''Output'''

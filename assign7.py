624d0a4781b4feb2159fca4a909653075e2acec9
prachichikshe <pchikshe12@gmail.com>
2017-06-09 12:33:25 +0530

    Create assign7.py

--- assign7.py
+++ assign7.py
@@ -1,5 +1,15 @@
+#Q 7. Write a Python program to read a file line by line store it into an array. 
+
 file_array = []
 with open('pc.txt',"r") as my_file:
     for line in my_file:
         file_array.append(line)
     print(file_array)
+    
+    
+'''Output
+['Paris is the capital and most populous city of France.\n', 
+"By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts.\n", 
+'It is the second largest metropolitan area in the European Union. \n', 
+'Paris is the fifth most expensive city in the world for luxury housing.\n']'''
+

f2972dc71bfe9a0ba95a50351c46e24525804cd4
prachichikshe <pchikshe12@gmail.com>
2017-06-09 15:54:03 +0530

    Create assign12.py

--- assign12.py
+++ assign12.py
@@ -1,7 +1,9 @@
 #Write a Python program to copy the contents of a file to another file . 
 
-from shutil import copyfile  
-copyfile('pc.txt', 'abc.txt')  
+from shutil import copyfile
+import os
+os.system('touch ab.txt')  
+copyfile('pc.txt', 'ab.txt')  
 
 '''Output
 '''

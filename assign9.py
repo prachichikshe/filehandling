c8fb229e5a4877ad199816ba627e77acd18ae6b4
prachichikshe <pchikshe12@gmail.com>
2017-06-09 16:08:24 +0530

    Create assign9.py

--- assign9.py
+++ assign9.py
@@ -1,7 +1,7 @@
 #Write a Python program to count the frequency of words in a file. 
  from collections import Counter
-    with open("pc.txt","r") as f:  
-                return Counter(f.read().split())  
+ with open("pc.txt","r") as f:  
+        return Counter(f.read().split())  
  print("Number of words in the file :",word_count("pc.txt"))  
  
  '''Output'''

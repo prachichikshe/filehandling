ab88c58b7fc9b986de40b2bb28697f300d1b4dae
prachichikshe <pchikshe12@gmail.com>
2017-06-09 12:30:39 +0530

    Create assign6.py

--- assign6.py
+++ assign6.py
@@ -3,5 +3,10 @@
 with open ("pc.txt", "r") as myfile:
     data=myfile.readlines()
     print(data)
+    
 
-'''Output'''
+'''Output
+['Paris is the capital and most populous city of France.\n',
+"By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts.\n",
+'It is the second largest metropolitan area in the European Union. \n', 
+'Paris is the fifth most expensive city in the world for luxury housing.\n']'''

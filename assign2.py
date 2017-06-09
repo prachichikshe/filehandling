105f59a482f6100f346c07a5b3334cd44beabeea
prachichikshe <pchikshe12@gmail.com>
2017-06-09 16:06:37 +0530

    Create assign2.py

--- assign2.py
+++ assign2.py
@@ -9,4 +9,6 @@ file_read_from_head('pc.txt',2)
 
 
 #----------------------------------
-'''Output'''
+'''Output
+Paris is the capital and most populous city of France.
+By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts.'''

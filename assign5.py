a457b6196388d54f6a4ae5deb880649020781d5c
prachichikshe <pchikshe12@gmail.com>
2017-06-09 12:45:51 +0530

    Create assign5.py

--- assign5.py
+++ assign5.py
@@ -1,8 +1,8 @@
 # Q5.Write a Python program to read a file line by line and store it into a list. 
  with open("pc.txt","r") as fo:
-           #Content_list is the list that contains the read lines.     
-            content_list = fo.readlines()
-            print(content_list)
+     #Content_list is the list that contains the read lines.     
+      content_list = fo.readlines()
+      print(content_list)
 
 
 '''Output

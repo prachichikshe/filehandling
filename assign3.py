30bf3ec3847509b61a99d64202817a3dcf5bb520
prachichikshe <pchikshe12@gmail.com>
2017-06-09 09:49:23 +0530

    Create assign3.py

--- assign3.py
+++ assign3.py
@@ -1,7 +1,14 @@
 # Write a Python program to append text to a file and display the text. 
+import io
+fo=open("pc.txt","w+")
+fo.write("\nHii..!!\nprachi\nhere")
+fo.close()
+fo=open("pc.txt","a")
+fo.write("appended file content\nbye")
+fo=open("pc.txt","r")
+str=fo.read()
+print str
+fo.close()
 
-fh = open(“pc.txt”, “a”) 
-fh.write(“We Meet Again World”) 
-fh.close() 
 
 '''Output'''
